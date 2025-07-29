09d291347c5db1275288e2a570eb0f130b862d45
George J. <19317503@brookes.ac.uk>
2025-07-30 04:15:59 +0530

    build: deployment to e2-vm google-cloud

--- service-portal/next.config.ts
+++ service-portal/next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
       fullUrl: true,
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 
 export default nextConfig;
